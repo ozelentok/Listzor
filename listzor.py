e19e7d07dfd95b03b6c7fafbf83be93bcaaae6f6
Oz Elentok <oz.elen@gmail.com>
2012-11-15 18:50:09 +0200

    inital commit

---
+++ listzor.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+#author: Oz Elentok
+#email: oz.elen@gmail.com
+#status: development
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+from parser import FileParser
+
+class MainForm:
+	def __init__(self):
+		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+		self.window.set_title("Listzor")
+		self.window.connect('delete_event', lambda w, e: gtk.main_quit())
+		self.window.set_default_size(500, 300)
+		self.treeview = gtk.TreeView()
+
+		self.treeview.set_reorderable(True)
+		self.scrollwindow = gtk.ScrolledWindow(
+			self.treeview.get_vadjustment(),
+			self.treeview.get_hadjustment())
+		self.scrollwindow.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+		self.scrollwindow.add(self.treeview)
+
+		self.vbox = gtk.VBox(False, 0)
+		self.vbox.pack_start(self.createMenu(), False, False, 0)
+		self.vbox.pack_start(self.scrollwindow, True, True, 0)
+				
+		self.window.add(self.vbox)
+		self.window.show_all()
+
+
+	def createMenu(self): 
+		menubar = gtk.MenuBar()
+ 	   
+		menu_file = gtk.Menu()
+		menu_edit = gtk.Menu()
+		menu_help = gtk.Menu()
+		
+		item_open = gtk.MenuItem("Open")
+		item_open.connect('button-press-event', self.openFile)
+		item_save = gtk.MenuItem("Save")
+		item_save.connect('button-press-event', self.saveFile)
+		item_quit = gtk.MenuItem("Quit")
+		item_quit.connect('button-press-event', lambda w, e: gtk.main_quit())
+		menu_file.append(item_open)
+		menu_file.append(item_save)
+		menu_file.append(item_quit)
+
+		item_cut = gtk.MenuItem("Cut")
+		item_copy = gtk.MenuItem("Copy")
+		item_paste = gtk.MenuItem("Paste")
+		menu_edit.append(item_cut)
+		menu_edit.append(item_copy)
+		menu_edit.append(item_paste)
+		
+		item_about = gtk.MenuItem("About")
+		menu_help.append(item_about)
+		
+		item_file = gtk.MenuItem("File")
+		item_edit = gtk.MenuItem("Edit")
+		item_help = gtk.MenuItem("Help")
+		
+		item_file.set_submenu(menu_file)
+		item_edit.set_submenu(menu_edit)
+		item_help.set_submenu(menu_help)
+		
+		menubar.append(item_file)
+		menubar.append(item_edit)
+		menubar.append(item_help)
+
+		return menubar
+
+	def openFile(self, widget, e):
+		fileChooser = gtk.FileChooserDialog("Open a File", self.window,
+				gtk.FILE_CHOOSER_ACTION_OPEN,
+				(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+				gtk.STOCK_OPEN, gtk.RESPONSE_OK), None)
+		fileFilter = gtk.FileFilter()
+		fileFilter.set_name('Text Files')
+		fileFilter.add_pattern('*.txt')
+		fileFilter.add_pattern('*.csv')
+		fileChooser.add_filter(fileFilter)
+		response = fileChooser.run()
+		if response != gtk.RESPONSE_OK:
+			fileChooser.destroy()
+			return
+		fileName = fileChooser.get_filename()
+		fileChooser.destroy()
+		fileParser = FileParser()
+		newListStore, tableHeaders = fileParser.parseToTable(fileName)
+
+		# TODO: add messege box for unsucessful file load
+		if newListStore == None or tableHeaders == None:
+			return
+
+		self.loadData(newListStore, tableHeaders)
+
+	def saveFile(self, widget, e):
+		fileChooser = gtk.FileChooserDialog("Save File", self.window,
+				gtk.FILE_CHOOSER_ACTION_SAVE,
+				(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+				gtk.STOCK_SAVE, gtk.RESPONSE_OK), None)
+		fileFilter = gtk.FileFilter()
+		fileFilter.set_name('Text Files')
+		fileFilter.add_pattern('*.txt')
+		fileFilter.add_pattern('*.csv')
+		fileChooser.add_filter(fileFilter)
+		response = fileChooser.run()
+		if response != gtk.RESPONSE_OK:
+			fileChooser.destroy()
+			return
+		fileName = fileChooser.get_filename()
+		fileChooser.destroy()
+		fileParser = FileParser()
+
+
+	def loadData(self, liststore, tableHeaders):
+
+		# remove old treeview columns
+		for col in self.treeview.get_columns():
+			self.treeview.remove_column(col)
+
+		# add new treeview columns
+		for i, header in enumerate(tableHeaders):
+			# cell
+			cell = gtk.CellRendererText()
+			cell.set_property("editable", True)
+			cell.connect("edited", self.cell_edited, liststore, i)
+			# column
+			col = gtk.TreeViewColumn(header, cell, text = i)
+			col.set_resizable(True)
+			col.set_sort_column_id(i)
+			self.treeview.append_column(col)
+
+			self.liststore = liststore;
+		self.treeview.set_model(self.liststore)
+
+	def cell_edited(self, widget, path, text, model, column):
+		model[path][column] = text
+
+	def main(self):
+		gtk.main()
+
+if __name__ == "__main__":
+	mainForm = MainForm()
+	mainForm.main()
+
---
+++ parser.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+
+#author: Oz Elentok
+#email: oz.elen@gmail.com
+#status: development
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import csv
+import gobject
+import sys
+
+class FileParser(object):
+
+	def __init__(self):
+		self.letterTypes = {'f':  float, 'i': int,
+				's': str, 'b': bool}
+		self.gobjectTypes = {gobject.TYPE_FLOAT: 'f', gobject.TYPE_INT: 'i'
+		 		,gobject.TYPE_STRING: 's', gobject.TYPE_BOOLEAN: 'b'}
+
+	def convertValToTableFormat(self, cellType, val):
+		if cellType == 'd':
+			return float(val)
+		elif cellType == 'i':
+			return int(val)
+		elif cellType == 'b':
+			if val == 'True':
+				return True
+			else:
+				return False
+		else:
+			return val
+	def convertTypeToFileFormat(self, colType):
+		return self.gobjectTypes[colType]
+
+	# parses file into table
+	# returns gtk.listStore containing the table, and returns the table headers
+	def parseToTable(self, filePath):
+		with open(filePath, 'r') as dataFile:
+			try:
+				dataRows = csv.reader(dataFile)
+				dataLetters = dataRows.next()
+				dataTypes = [self.letterTypes[t] for t in dataLetters]
+				liststore = gtk.ListStore(*dataTypes)
+				tableHeaders = dataRows.next()
+				for row in dataRows:
+					# row is empty
+					if not row:
+						continue
+					data = []
+					for i, cellType in enumerate(dataLetters):
+						val = self.convertValToTableFormat(cellType, row[i])
+						data.append(val)
+					liststore.append(data)
+				return liststore, tableHeaders
+
+			except:
+				print >> sys.stderr, 'File Data is malformed'
+		return None, None
+
+	# parses data into csv file
+	def parseToFile(self, filePath, liststore, tableHeaders):
+		with open(filePath, 'w',) as dataFile:
+			csvWriter = csv.writer(dataFile)
+			dataTypes = []
+			for i in range(liststore.get_n_columns()):
+				col = liststore.get_column_type(i)
+				colLetter = self.convertTypeToFileFormat(col)
+				dataTypes.append(colLetter)
+			csvWriter.writerow(dataTypes)
+			csvWriter.writerow(tableHeaders)
+
+			for row in liststore:
+				csvWriter.writerow(row)
